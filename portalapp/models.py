84e75370f3489fd3a95fa47692523d311ee78724
Chetan Kumar <sharma2232@email.com>
2025-04-27 12:47:07 +0530

    Added header comment in models.py by Chetan Kumar

--- portalapp/models.py
+++ portalapp/models.py
@@ -1,6 +1,5 @@
-from django.db import models
+# Models for Notice Management System - Updated by Chetan Kumar
 
-# Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
 
