58287939c13fe1ea65f42d56198e1bdf1cdad23f
Sehajpreet Singh <sehajsandhu-1@email.com>
2025-04-27 14:19:27 +0530

    Updated views.py by Sehajpreet Singh

--- portalapp/views.py
+++ portalapp/views.py
@@ -1,3 +1,5 @@
+# Views for Medical Staff Portal - Edited by Sehajpreet Singh
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
